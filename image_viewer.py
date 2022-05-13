eae8c5aaa1bf089a235063dc23202515792a0f9f
johnobogne <johnobognee@gmail.com>
2022-05-13 18:38:39 +0800

    add status bar

--- image_viewer.py
+++ image_viewer.py
@@ -32,12 +32,16 @@ def display(counter):
     global imageDisplay
     imageDisplay = Label(image=images[counter])
     imageDisplay.grid(row=0, column=0, columnspan=3)
+
+    status = Label(root, text='Image ' + str((currentImage +1 )) + ' of ' + str(len(images)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
     root.eval('tk::PlaceWindow . center')
 
 currentImage = 0
 
 backButton = Button(root, text='<', command=lambda: previous()).grid(row=1, column=0)
-nextButton = Button(root, text='>', command=lambda: next()).grid(row=1, column=2)
+nextButton = Button(root, text='>', command=lambda: next()).grid(row=1, column=2, pady=10)
 exitButton = Button(root, text='Close', command=root.quit).grid(row=1, column=1)
 
 display(currentImage)
